db165f43ee5cb3a013ea73d5a5a1a0a953029cdf
MdNoorr <nurmuhammadd2001@gmail.com>
2020-12-26 14:11:48 +0600

    done

---
+++ PythonProjects/audio.py
@@ -0,0 +1,6 @@
+# print(1232)
+import pyttsx3
+friend = pyttsx3.init()
+friend.say("I can speak now")
+friend.runAndWait()
+
---
+++ PythonProjects/heart2.py
@@ -0,0 +1,13 @@
+from turtle import *
+color("blue")
+begin_fill()
+
+left(50)
+forward(250)
+circle(40, 180)
+
+left(260)
+circle(40, 180)
+forward(250)
+end_fill()
+done()
